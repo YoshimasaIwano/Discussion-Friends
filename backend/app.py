ff7ed29fc99338aa0f3761bc8d4c8ffe822c5458
YoshimasaIwano <44yoshimasa03@gmail.com>
2023-04-28 17:04:12 -0700

    add GCP config

--- backend/app.py
+++ backend/app.py
@@ -10,14 +10,14 @@ def index():
     return app.send_static_file('index.html')
 
 
-# # this is for deployment
-# if __name__ == "__main__":
-#     app.debug = False
-#     PORT = os.environ.get('PORT', '5000')
-#     serve(app, host='0.0.0.0', port=PORT)
-
-# this is for your local environment
+# this is for deployment
 if __name__ == "__main__":
-    app.debug = True
+    app.debug = False
     PORT = os.environ.get('PORT', '5000')
-    app.run()
+    serve(app, host='0.0.0.0', port=PORT)
+
+# # this is for your local environment
+# if __name__ == "__main__":
+#     app.debug = True
+#     PORT = os.environ.get('PORT', '5000')
+#     app.run()
