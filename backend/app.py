078f2bc4eea7b588ba06fc1a6c862763b2872ac6
Kaiyu0128 <yokoikaiyu0128@gmail.com>
2023-04-28 20:05:31 -0700

    delete confliction

--- backend/app.py
+++ backend/app.py
@@ -21,20 +21,8 @@ if __name__ == "__main__":
     PORT = os.environ.get('PORT', '5000')
     serve(app, host='0.0.0.0', port=PORT)
 
-# this is for deployment
-if __name__ == "__main__":
-    app.debug = False
-    PORT = os.environ.get('PORT', '5000')
-    serve(app, host='0.0.0.0', port=PORT)
-
 # # this is for your local environment
 # if __name__ == "__main__":
 #     app.debug = True
 #     PORT = os.environ.get('PORT', '5000')
 #     serve(app, host='0.0.0.0', port=PORT)
-
-# this is for your local environment
-if __name__ == "__main__":
-    app.debug = True
-    PORT = os.environ.get('PORT', '5000')
-    app.run()
